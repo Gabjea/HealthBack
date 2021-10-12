626c2618fefec6cbde8bf0bf6e3c68c826225a78
Gabjea <gabjea856@gmail.com>
2021-10-13 00:46:43 +0300

    -auth

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+
 require("dotenv").config();
 require("./auth/passport");
 
@@ -13,13 +13,13 @@ const api = require("./api");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.json({
